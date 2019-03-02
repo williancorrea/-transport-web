f5529121d9889a1f15c1ef74095f2b198269a5a1
Willian Vagner Vicente Correa <willian.vag@gmail.com>
2019-03-02 19:31:21 -0300

    Teste CORs

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.get('/*', function (req, res) {
+   res.header("Access-Control-Allow-Origin", '*');
+   res.header("Access-Control-Allow-Credentials", true);
+   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+   res.sendFile(__dirname + '/dist/index.html');
+
 });
 
 app.listen(process.env.PORT || 4200);
